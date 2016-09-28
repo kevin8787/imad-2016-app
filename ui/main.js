05c0de0f9d082865c8622b096fa69bfc1a6266c4
kevin8787 <kevink.abey6@gmail.com>
2016-09-29 01:02:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element=document.getElementById('main-text');
 element.innerHTML=('Dinosure with Laptop');
 
 //move the image
-var img=document.getElementById('madi');
+var img=document.getElementById('img');
 img.onclick = function(){
     img.style.marginleft = '100px';
 };
