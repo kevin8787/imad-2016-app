599540055f6eda139ce2d011b8b8164dec1ab407
kevin8787 <kevink.abey6@gmail.com>
2016-09-29 01:12:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,6 +120,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
